6ffff3dc3725859bb9eb3db095cb8a600248b1de
lucasfmerino <lucasfmerino@gmail.com>
2023-11-01 20:52:40 -0300

    java basic - conditionals - part 01

---
+++ src/Conditional.java
@@ -0,0 +1,20 @@
+public class Conditional {
+    public static void main(String[] args) {
+        int releaseYear = 2022;
+        boolean includedInThePlan = false;
+        double filmReviewScore = 8.1;
+        String planType = "plus";
+
+        if (releaseYear >= 2022) {
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir");
+        }
+
+        if (includedInThePlan || planType.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Para assistir à este filme é necessáio assinar o plano \"PLUS\"");
+        }
+    }
+}
